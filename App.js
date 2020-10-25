f0d3999814fd68ba8a92205dc6ed3615c37fd484
Jonagarme <garciajonathan268@gmail.com>
2020-10-25 17:29:22 -0500

    terminado

--- App.js
+++ App.js
@@ -1,59 +1,50 @@
 import React from 'react';
-import {StyleSheet, View, Linking, Image, ScrollView} from 'react-native';
-import {Text, Button} from 'react-native-elements';
+import {
+  StyleSheet,
+  View,
+  Linking,
+  Dimensions,
+  ImageBackground,
+} from 'react-native';
+import {Button} from 'react-native-elements';
 
-//import logo from './img/logo.png';
-import logo from './img/log.jpg';
+import logo2 from './img/logo.png';
+
+const {width} = Dimensions.get('window');
 
 export default function App() {
   return (
-    <ScrollView style={{backgroundColor: '#ddb150'}}>
-      <View>
-        <Image
-          source={logo}
-          style={{
+    <View style={styles.container}>
+      <ImageBackground source={logo2} style={styles.image}>
+        <Button
+          title="Da Click!!!"
+          onPress={() => Linking.openURL('https://www.planburger.com.co')}
+          titleStyle={{color: '#000000'}}
+          type="clear"
+          buttonStyle={{
+            borderColor: '#212121',
+            borderRadius: 20,
+            borderWidth: 1,
+            width: 200,
+            height: 40,
             alignSelf: 'center',
-            marginTop: 20,
-            height: 500,
-            width: 500,
+            marginTop: 220,
           }}
+          containerStyle={{backgroundColor: 'transparent'}}
         />
-
-        <View style={{alignSelf: 'center', marginTop: 30}}>
-          <Button
-            title="Da Click!!!"
-            onPress={() => Linking.openURL('https://www.planburger.com.co')}
-            titleStyle={{color: '#000000'}}
-            type="clear"
-            buttonStyle={{
-              borderColor: '#212121',
-              borderRadius: 20,
-              borderWidth: 1,
-              width: 200,
-              height: 40,
-            }}
-            containerStyle={{backgroundColor: 'transparent'}}
-          />
-        </View>
-      </View>
-    </ScrollView>
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  texto: {
-    fontSize: 25,
-    textAlign: 'center',
-    margin: 35,
-
-    textTransform: 'uppercase',
+  container: {
+    flex: 1,
+    flexDirection: 'column',
   },
-  titulo: {
-    textAlign: 'center',
-    color: '#e64a19',
-    fontSize: 50,
-    fontWeight: 'bold',
-    marginTop: 20,
-    textTransform: 'uppercase',
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
   },
 });
